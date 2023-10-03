097f0b5a47e4d884e97aff371d983b4a4078d70c
ajay sathvik <ajaysathvikk@gmail.com>
2023-10-03 14:47:02 +0530

    landing page complete

---
+++ my-app/src/App.js
@@ -0,0 +1,13 @@
+import LandingPage from "./pages/LandingPage";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <LandingPage />
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ my-app/src/pages/LandingPage.js
@@ -0,0 +1,72 @@
+import React from "react";
+import icon from "./assets/Screenshot_2023-09-20_201047-removebg-preview.png";
+import "./LandingPage.css";
+import metamask from "./assets/Untitled_design__9__1-removebg-preview.png";
+import blockdiagram from "./assets/original-4cfe1ccf4ff167f1e18b27afe33c1f5d 1.png";
+import moonpic from "./assets/moon (1) 2.png";
+import { Link } from "react-router-dom";
+
+
+const LandingPage = () => {
+  return (
+    <div className="landing">
+      <div className="navbar">
+        <img className="icon" alt="icon" src={icon}></img>
+        <div className="heading">TrustVault</div>
+        <div className="aboutbox">
+          <div className="about">About</div>
+        </div>
+        <div className="colourmode">
+          <img src={moonpic} alt="moon" className="moon"></img>
+        </div>
+        <div className="connect">
+          <img src={metamask} alt="metamask" className="metamask" />
+          <div className="connectbutton">
+            <div className="Connect">Connect</div>
+          </div>
+        </div>
+      </div>
+      <div className="main">
+        <li>
+        <Link to="/">Home</Link>
+        </li>
+        <div className="welcomecontent">
+          Easy and Secure access to your Document
+        </div>
+        <img src={blockdiagram} alt="blockdiagram" className="blockdiagram" />
+        <button className="learnmore">Learn More</button>
+        <div className="subheading">
+          Services We <br />
+          Provide
+        </div>
+        <div className="servicesweprovide">
+          <div className="serviceno1">
+            Secure Blockchain-Based Legal Record Storage
+          </div>
+          <div className="serviceno1content">
+            Our Blockchain Security service provides advanced encryption and
+            authentication techniques to safeguard your digital assets and
+            transactions.
+          </div>
+          <div className="serviceno2">
+            Secure and Transparent Legal Record Storage{" "}
+          </div>
+          <div className="serviceno2content">
+            Legal eVault is a secure online platform that allows individuals and
+            businesses to store and manage their important legal documents.
+          </div>
+          <div className="serviceno3">
+            Secure and Transparent Legal Record Storage
+          </div>
+          <div className="serviceno3content">
+            Record Accessibility provides a secure and efficient way to access
+            and manage important records with a formal tone.
+          </div>
+        </div>
+        <div className="footer"></div>
+      </div>
+    </div>
+  );
+};
+
+export default LandingPage;
